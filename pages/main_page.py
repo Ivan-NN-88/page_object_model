e0a387619e7c62a2c10d48837b755dbc0cb88ba8
Ivan-NN-88 <moidocumenti88@mail.ru>
2021-07-25 19:23:40 +0300

    4.2.2 - Тест на основе Page Object. Создал тест "MainPage" на основе ООП.

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,5 +7,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     """Page Object для главной страницы сайта."""
     def go_to_login_page(self):
+        """Переходит на страницу авторизации."""
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
