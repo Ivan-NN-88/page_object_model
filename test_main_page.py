14630d17547443fd365cff82f02643cfab0f95d1
Ivan-NN-88 <moidocumenti88@mail.ru>
2021-07-26 18:13:39 +0300

    4.2.8 - Реализация LoginPage.

--- test_main_page.py
+++ test_main_page.py
@@ -1,20 +1,27 @@
 """Тест на основе Page Object."""
 
 from .pages.main_page import MainPage
-
-
-LINK = 'http://selenium1py.pythonanywhere.com/'
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     """Тест на основе Page Object."""
-    page = MainPage(browser, LINK)
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
     """Тест должен увидеть ссылку на страницу авторизации."""
-    page = MainPage(browser, LINK)
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_be_login_page(browser):
+    """Тест самой страницы авторизации на селектора."""
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
